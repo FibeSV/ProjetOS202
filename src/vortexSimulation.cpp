68b5bd8418ff82dd134186296a19a798f681bd6e
Semyon <slowmouker@gmail.com>
2023-03-18 21:08:53 +0100

    some

--- src/vortexSimulation.cpp
+++ src/vortexSimulation.cpp
@@ -94,20 +94,22 @@ auto readConfigFile( std::ifstream& input ) {
 
 int main( int nargs, char* argv[] )
 {
-    MPI_Comm global;
-    int rank, nbp;
+    // MPI initialization
+    MPI_Comm global;    // MPI communicator
+    int rank, nbp;      // MPI rank and number of processes
 
     MPI_Init(&nargs, &argv);
-    MPI_Comm_dup(MPI_COMM_WORLD, &global);
-    MPI_Comm_size(global, &nbp);
-    MPI_Comm_rank(global, &rank);
+    MPI_Comm_dup(MPI_COMM_WORLD, &global);  // Duplicate the communicator to get a local communicator
+    MPI_Comm_size(global, &nbp);            // Get the total number of processes
+    MPI_Comm_rank(global, &rank);           // Get the rank of the current process
 
-    MPI_Datatype MPI_Point;
+    MPI_Datatype MPI_Point;                 // Declare an MPI datatype for Point object
 
-    int          len[2] = { 1, 1 };
-    MPI_Aint     pos[2] = { offsetof(Geometry::Point<double>, x), offsetof(Geometry::Point<double>, y)};
-    MPI_Datatype typ[2] = { MPI_DOUBLE, MPI_DOUBLE };
+    int          len[2] = { 1, 1 };         // Size of each element of the struct in the datatype
+    MPI_Aint     pos[2] = { offsetof(Geometry::Point<double>, x), offsetof(Geometry::Point<double>, y)};  // Offset of each element of the struct in the datatype
+    MPI_Datatype typ[2] = { MPI_DOUBLE, MPI_DOUBLE };  // Type of each element of the struct in the datatype
 
+    // Create the MPI datatype for Point object
     MPI_Type_create_struct( 2, len, pos, typ, &MPI_Point );
     MPI_Type_commit( &MPI_Point );
 
@@ -120,27 +122,26 @@ int main( int nargs, char* argv[] )
 
     filename = argv[1];
     std::ifstream fich(filename);
-    auto config = readConfigFile(fich);
+    auto config = readConfigFile(fich);     // Read configuration from file
     fich.close();
-
-
 
     std::size_t resx=800, resy=600;
     if (nargs>3)
     {
-        resx = std::stoull(argv[2]);
+        resx = std::stoull(argv[2]);        // Get the resolution of the screen from command line argument
         resy = std::stoull(argv[3]);
     }
 
-    auto vortices = std::get<0>(config);
-    auto isMobile = std::get<1>(config);
-    auto grid     = std::get<2>(config);
-    auto cloud    = std::get<3>(config);
+    auto vortices = std::get<0>(config);   // Get the list of vortices from configuration
+    auto isMobile = std::get<1>(config);   // Get whether the vortices are mobile or not from configuration
+    auto grid     = std::get<2>(config);   // Get the grid information from configuration
+    auto cloud    = std::get<3>(config);   // Get the cloud information from configuration
     
-    grid.updateVelocityField(vortices);
+    grid.updateVelocityField(vortices);     // Update the velocity field of the grid using the vortices
 
     double dt = 0.1;
     if (rank == INTERFACE_RANK){
+        // Code for the interface process
         std::cout << "######## Vortex simultor ########" << std::endl << std::endl;
         std::cout << "Press P for play animation " << std::endl;
         std::cout << "Press S to stop animation" << std::endl;
